7bb2db06baa539922bde8ba46ac6342e05e04f01
Damian Lobos <damianlobos24@gmail.com>
2024-08-16 13:54:08 -0300

    Intro video added

--- MimicAlienCakeInvation/Assets/Scripts/MenuScripts/UIMenuController.cs
+++ MimicAlienCakeInvation/Assets/Scripts/MenuScripts/UIMenuController.cs
@@ -8,7 +8,10 @@ public class UIMenuController : MonoBehaviour
     [SerializeField] private GameObject anyKeyMenu;
     [SerializeField] private GameObject elevatorAnimMenu;
     [SerializeField] private GameObject mainMenu;
-    [SerializeField] private VideoPlayer vp;
+    [SerializeField] private GameObject intro;
+    [SerializeField] private AudioSource audio;
+    private VideoPlayer vp;
+
 
     private void Update()
     {
@@ -22,23 +25,40 @@ public class UIMenuController : MonoBehaviour
             if (Input.anyKey)
             {
                 elevatorAnimMenu.SetActive(true);
+                vp = GameObject.Find("DoorAnimation").GetComponent<VideoPlayer>();
             }
         }
-        if (vp.isPlaying)
+        if (vp.isPlaying  && elevatorAnimMenu.activeSelf)
         {
             anyKeyMenu.SetActive(false);
             vp.loopPointReached += CheckEndOfVideo;
         }
+        if (vp.isPlaying && intro.activeSelf)
+            mainMenu.SetActive(false);
     }
+
+    
 
     private void CheckEndOfVideo(VideoPlayer vp)
     {
-        Debug.Log("Termino");
         mainMenu.SetActive(true);
         elevatorAnimMenu.SetActive(false);
     }
 
     public void OnClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void onStartClick()
+    {
+        intro.SetActive(true);
+        vp = GameObject.Find("IntroVideo").GetComponent<VideoPlayer>();
+        vp.loopPointReached += StartGame;
+        audio.Stop();
+    }
+
+    private void StartGame(VideoPlayer vp)
     {
         Application.Quit();
     }
